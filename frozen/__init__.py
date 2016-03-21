d2db2b34d6a3d5c795aed5672736d597bd44665f
rafiki <mark@rafiki>
2016-03-21 18:50:29 +0100

    seems immutable

---
+++ frozen/__init__.py
@@ -0,0 +1,4 @@
+
+from .proxy import ImmutableProxy, ImmutableError
+
+
--- frozen/proxy.py
+++ frozen/proxy.py
@@ -1,5 +1,10 @@
 
+import operator
 from lazy_object_proxy import Proxy
+
+
+class ImmutableError(Exception):
+	""" Attempted to change an immutable object. """
 
 
 class ImmutableProxy(Proxy):
@@ -7,19 +12,69 @@ class ImmutableProxy(Proxy):
 	Immutable proxy; any attempted changes raise an exception (underlying object can still be changed by those that have a reference).
 	"""
 	def __setattr__(self, name, value, __setattr__=object.__setattr__):
-		if hasattr(type(self), name):
-			__setattr__(self, name, value)
-		else:
-			setattr(self.__wrapped__, name, value)
+		raise ImmutableError('object "{0:}" is frozen; attributes cannot be assigned'.format(self))
 
 	def __delattr__(self, name, __delattr__=object.__delattr__):
-		if hasattr(type(self), name):
-			__delattr__(self, name)
+		raise ImmutableError('object "{0:}" is frozen; attributes cannot be deleted'.format(self))
+
+	def __setitem__(self, key, value):
+		raise ImmutableError('object "{0:}" is frozen; items cannot be assigned'.format(self))
+
+	def __delitem__(self, key):
+		raise ImmutableError('object "{0:}" is frozen; items cannot be deleted'.format(self))
+
+	def __setslice__(self, i, j, value):
+		raise ImmutableError('object "{0:}" is frozen; slices cannot be assigned'.format(self))
+
+	def __delslice__(self, i, j):
+		raise ImmutableError('object "{0:}" is frozen; slices cannot be deleted'.format(self))
+
+	def __iadd__(self, other):
+		return self.__wrapped__ + other
+
+	def __isub__(self, other):
+		return self.__wrapped__ - other
+
+	def __imul__(self, other):
+		return self.__wrapped__ * other
+
+	def __idiv__(self, other):
+		return operator.div(self.__wrapped__, other)
+
+	def __itruediv__(self, other):
+		return operator.truediv(self.__wrapped__, other)
+
+	def __ifloordiv__(self, other):
+		return self.__wrapped__ // other
+
+	def __imod__(self, other):
+		return self.__wrapped__ ^ other
+
+	def __ipow__(self, other, *args):
+		return pow(self.__wrapped__, other, *args)
+
+	def __ilshift__(self, other):
+		return self.__wrapped__ << other
+
+	def __irshift__(self, other):
+		return self.__wrapped__ >> other
+
+	def __iand__(self, other):
+		return self.__wrapped__ & other
+
+	def __ixor__(self, other):
+		return self.__wrapped__ ^ other
+
+	def __ior__(self, other):
+		return self.__wrapped__ | other
 
 
-class DetachedProxy(Proxy):
-	"""
-	Changes are stored on the proxy but don't change the original instance (underlying object can still be changed by those that have a reference).
-	"""
+#todo: if type(myval) in (int, float, bool, str ...): function
+
+
+# class DetachedProxy(Proxy):
+# 	"""
+# 	Changes are stored on the proxy but don't change the original instance (underlying object can still be changed by those that have a reference).
+# 	"""
 
 
